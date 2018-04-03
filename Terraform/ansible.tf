249d5c9d94699d51f86be32d9c7aa2c5fbdb2047
Robert J <robert@scriptmyjob.com>
2018-04-02 20:06:39 -0500

    Renamed ansible roles to be more generic in preperation to switch to MySQL.

--- Terraform/ansible.tf
+++ Terraform/ansible.tf
@@ -1,20 +1,10 @@
-resource "null_resource" "start_sleep" {
-    depends_on = [
+resource "null_resource" "ansible_exec" {
+    depends_on =[
         "aws_autoscaling_group.DB_Cluster"
     ]
 
     provisioner "local-exec" {
-        command = "sleep 10"
-    }
-}
-
-resource "null_resource" "ansible_exec" {
-    depends_on =[
-        "null_resource.start_sleep"
-    ]
-
-    provisioner "local-exec" {
-        command     = "echo 'This command doesn't matter but I must put it in for some reason'"
+        command     = " "
         interpreter = ["/bin/bash", "./local-exec-ansible.sh"]
     }
 }
